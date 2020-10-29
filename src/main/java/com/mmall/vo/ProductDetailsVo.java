ffea8f1c5f593c890649c8a51f9d3dda67882ec1
GuoQingDeng <1255826318@qq.com>
2020-10-29 16:12:15 +0800

    Add ProductDetailsVo

--- src/main/java/com/mmall/vo/ProductDetailsVo.java
+++ src/main/java/com/mmall/vo/ProductDetailsVo.java
@@ -1,4 +1,141 @@
 package com.mmall.vo;
 
+import java.math.BigDecimal;
+
+/**
+ * 添加VO对象，以前都是直接返回给前端的POJO对象
+ * 但是有时候，返回POJO不能满足需求，比如说，需要对数据做一些转换（时间戳转换成可视时间），
+ * 添加新的字段数据等
+ * 这时候就需要VO对象 ==> VO : Value Object
+ */
 public class ProductDetailsVo {
+
+    private Integer  id;
+    private Integer categoryId;
+    private String name;
+    private String subtitle;
+    private String mainImage;
+    private String subImages;
+    private String detail;
+    private BigDecimal price;
+    private Integer stock;
+    private Integer status;
+    private String createTime;
+    private String updateTime;
+
+
+    private String imageHost; // 存放图片的FTP服务器地址
+    private Integer parentCategoryId;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Integer categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSubtitle() {
+        return subtitle;
+    }
+
+    public void setSubtitle(String subtitle) {
+        this.subtitle = subtitle;
+    }
+
+    public String getMainImage() {
+        return mainImage;
+    }
+
+    public void setMainImage(String mainImage) {
+        this.mainImage = mainImage;
+    }
+
+    public String getSubImages() {
+        return subImages;
+    }
+
+    public void setSubImages(String subImages) {
+        this.subImages = subImages;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public Integer getStock() {
+        return stock;
+    }
+
+    public void setStock(Integer stock) {
+        this.stock = stock;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public String getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(String createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(String updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public String getImageHost() {
+        return imageHost;
+    }
+
+    public void setImageHost(String imageHost) {
+        this.imageHost = imageHost;
+    }
+
+    public Integer getParentCategoryId() {
+        return parentCategoryId;
+    }
+
+    public void setParentCategoryId(Integer parentCategoryId) {
+        this.parentCategoryId = parentCategoryId;
+    }
 }
