fd329d3e2d236efbb524db2ffca97ab845cdd6cf
GuoQingDeng <1255826318@qq.com>
2020-10-29 16:29:52 +0800

    Add DateTimeUtil, use the Joda-time for time conversion

---
+++ src/main/java/com/mmall/util/DateTimeUtil.java
@@ -0,0 +1,78 @@
+package com.mmall.util;
+
+import org.apache.commons.lang3.StringUtils;
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
+import java.util.Date;
+
+/**
+ * 时间处理工具类
+ * 使用Joda-time。
+ *  1. str->Date 字符串转Date
+ *  2. Date->str Date转字符串
+ */
+public class DateTimeUtil {
+
+    public static final String STANDARD_FORMAT = "yyyy-MM-dd HH:mm:ss";
+
+    /**
+     * 字符串转Date，需要指定格式
+     *
+     * @param dateTimeStr
+     * @param formatStr  指定格式
+     * @return
+     */
+    public static Date strToDate(String dateTimeStr, String formatStr){
+        DateTimeFormatter dateTimeFormatter = DateTimeFormat.forPattern(formatStr);
+        DateTime dateTime = dateTimeFormatter.parseDateTime(dateTimeStr);
+        return dateTime.toDate();
+    }
+
+    /**
+     * Date转字符串,需要指定格式
+     *
+     * @param date
+     * @param formatStr 指定格式
+     * @return
+     */
+    public static String dateToStr(Date date,String formatStr){
+        if(date == null){
+            return StringUtils.EMPTY;
+        }
+        DateTime dateTime = new DateTime(date);
+        return dateTime.toString(formatStr);
+    }
+
+    /**
+     * 字符串转Date, 使用标准格式
+     *
+     * @param dateTimeStr
+     * @return
+     */
+    public static Date strToDate(String dateTimeStr){
+        DateTimeFormatter dateTimeFormatter = DateTimeFormat.forPattern(STANDARD_FORMAT);
+        DateTime dateTime = dateTimeFormatter.parseDateTime(dateTimeStr);
+        return dateTime.toDate();
+    }
+
+    /**
+     * Date转字符串, 使用标准格式
+     * @param date
+     * @return
+     */
+    public static String dateToStr(Date date){
+        if(date == null){
+            return StringUtils.EMPTY;
+        }
+        DateTime dateTime = new DateTime(date);
+        return dateTime.toString(STANDARD_FORMAT);
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(DateTimeUtil.dateToStr(new Date(),"yyyy-MM-dd HH:mm:ss"));
+        System.out.println(DateTimeUtil.strToDate("2010-01-01 11:11:11","yyyy-MM-dd HH:mm:ss"));
+    }
+}
